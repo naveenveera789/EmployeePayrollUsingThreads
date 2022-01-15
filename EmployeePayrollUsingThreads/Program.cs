43bf011d89bbf42ac7bd21499737f4033d800836
naveenveera789 <naveenveera789@gmail.com>
2022-01-15 16:36:47 +0530

    [naveenveera789] add : The addition of multiple records without using thread is done

---
+++ EmployeePayrollUsingThreads/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace EmployeePayrollUsingThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+        }
+    }
+}
